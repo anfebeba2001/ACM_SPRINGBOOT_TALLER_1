517f06b295abc6842898c3c0a2da82530cee4315
Andrés <anfebeba2001@gmail.com>
2025-05-29 17:18:49 -0500

    init

---
+++ PrimerTaller/src/test/java/com/ACM/PrimerTaller/PrimerTallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ACM.PrimerTaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerTallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
